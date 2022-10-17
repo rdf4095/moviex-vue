04aad513e101d960d3a26188c955750d47a6ebe5
rdf4095 <112559769+rdf4095@users.noreply.github.com>
2022-10-16 22:49:37 -0400

    compute classes for div and figures

--- src/components/ShowImageRow.vue
+++ src/components/ShowImageRow.vue
@@ -1,43 +1,60 @@
 <template>
-  <div v-if="haveImages" :class="[ 'image-row', {'allow-text-flow': classObject.stacked}]">
-    <figure :class="classObject">
-      <img :src="getImgUrl( event.images[ imgarray[0]-1 ] )">
-      <figcaption id="caption1">{{ event.captions[ imgarray[0]-1 ] }}</figcaption>
+  <div v-if="haveImages" :class="[ 'image-row',
+                                   classDiv ]">
+    <figure :class="[ 'basic', classFigure ]">
+      <img :src="getImgUrl( images[0] )">
+      <figcaption id="caption1">{{ captions[0] }}</figcaption>
     </figure>
-    <figure v-if="secondImage" :class="classObject">
-      <img :src="getImgUrl( event.images[ imgarray[1]-1 ] )">
-      <figcaption id="caption2">{{ event.captions[ imgarray[1]-1 ] }}</figcaption>
+    <figure v-if="secondImage" :class="[ 'basic', classFigure ]">
+      <img :src="getImgUrl( images[1] )">
+      <figcaption id="caption2">{{ captions[1] }}</figcaption>
     </figure>
-    <!-- debug
-      <p>images: {{ event.images }}</p>
-      <p>captions: {{ event.captions }}</p>
-      <p>imgarray: {{imgarray}}</p>
-      <p>imgarray: {{ imgarray[0]}}, {{imgarray[1] }}</p>
-    -->
   </div>  
 </template>
 
 
 <script>
+/*
+  <div v-if="haveImages" :class="[ 'image-row',
+                                   {'textleft': classObject}]">
+*/
 export default {
-    // props: ["event", "imgarray", "arrange", "orient"],
     props: {
         event: {
             type: Object
         },
         imgarray: {
-            type: Array,
+            type: Array
         },
         arrange: {
+            // sidebyside, stacked
             type: String,
-            default: "sidebyside"
+            default: "stacked"
         },
-        orient: {
+        textflow: {
+            // left, right
             type: String,
+            default: "right"
         }
     },
 
     computed: {
+        images: function() {
+            // create zero-based array of image filenames
+            let arr = [];
+
+            this.imgarray.forEach(e => arr.push(this.event.images[e-1]));
+
+            return arr;
+        },
+        captions: function() {
+            // create zero-based array of captions
+            let arr = [];
+
+            this.imgarray.forEach(e => arr.push(this.event.captions[e-1]));
+
+            return arr;
+        },
         secondImage: function() {
             return (this.imgarray.length == 2) ? true : false;
         },
@@ -50,11 +67,17 @@ export default {
 
             return havesome;
         },
-        classObject() {
+        classDiv() {
+            return {
+                // basic: true,
+                textright: (this.textflow == "right") ? true : false,
+                textleft: (this.textflow == "left") ? true: false
+            }
+        },
+        classFigure() {
             return {
                 basic: true,
-                sidebyside: (this.arrange == "sidebyside") ? true : false,
-                stacked: (this.arrange == "stacked") ? true : false
+                sidebyside: (this.arrange == "sidebyside") ? true : false
             }
         }
     },
@@ -62,8 +85,6 @@ export default {
     data() {
         return {
             // classes for image arrangement
-            // imgStacked: false,
-            // divHoriz: (this.classObject.sidebyside) ? true : false
         }
     },
 
@@ -79,50 +100,50 @@ export default {
 
 
 <style scoped>
-/*  TODO:
-  Handle options using classes:
-    1. OK: images close to 4x3 ratio,
-      a. OK: display side-by-side at left, and allow text wrap.
-      b. OK: display one-above-other at left, and allow text wrap.
-    2. images close to 2.5x1, display one-above-other at left, and allow text wrap.
-    3. large images, center with no text wrap.
-    4. medium vertical images, display at right, and allow text wrap.
-    5. for cases 1 and 2, specify width variable in each view file (views/*.vue)
-*/
-
 /*
-  div.image_row does not expand with its content, but that's ok in this case, as it
-  is a simple container as required by the vue template.
+  div.image_row does not expand with its content, but that's ok in this case: it is
+  a container as required by the vue template, but serves no other purpose.
   There are several ways to achieve div expansion, but they all have side effects 
   on text flow and/or object alignment.
 */
 
-/* is this "basic" ??  */
 div.image-row {
-  /* push down the text immediately below the container: helps readability and 
-     better offsets the image from the text.  Doesn't seem to matter what the value 
-     is, just that a value is specified.
-  */
-  margin-bottom: 1em;
+  /* float: left; */
+  /* margin-right: 1.75em; */
+  width: fit-content;
 }
-.allow-text-flow {
+.textleft {
+  float: right;
+  margin-left: 1.5em;
+}
+.textright {
   float: left;
+  margin-right: 1.5em;
 }
 
+
 figure.basic {
-  margin: 0 1.5em 1em 0;
+  /* float: left; */
+  margin: 0 0.5em 1.25em 0;
   border: 1px solid lightgrey;
   box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.35);
   -moz-box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.35);
   -webkit-box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.35);
 }
-.sidebyside {
-  float: left;
+
+figure.sidebyside {
+  display: inline-block;
 }
-.stacked {
+
+
+figure.stacked {
   width: fit-content;
   width: -moz-fit-content;
   width: -webkit-fit-content;
+}
+figure.right {
+  /* float: right; */
+  margin: 0 0.25em 1em 1em;
 }
 
 figcaption {
@@ -139,4 +160,16 @@ img {
   vertical-align: top;
   background: white;
 }
+/*  TODO:
+  Handle options using classes:
+    1. OK: images close to 4x3 ratio,
+      a. OK: display side-by-side at left, and allow text wrap.
+      b. OK: display one-above-other at left, and allow text wrap.
+    2. images close to 2.5x1, display one-above-other at left, and allow text wrap.
+    3. large images, center with no text wrap.
+    4. medium vertical images, display at right, and allow text wrap.
+    5. consider a prop to change the image width (will need more width variables)
+    6. consider title above the set of images
+    7. consider v-for for figures (based on images[n])
+*/
 </style>
